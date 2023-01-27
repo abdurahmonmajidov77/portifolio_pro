572b88371b48c504ed368532926b7b9feae3e7ed
Abdurahmon Majidov <majidovabdurahmon573@gmail.com>
2023-01-27 22:40:52 +0500

    Change

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,5 +1,6 @@
 import './style.css'
 import SideImg from '../../assets/Abdurahmon.jpeg'
+import { NavLink } from 'react-router-dom';
 
 function Sidebar() {
   return (
@@ -7,11 +8,11 @@ function Sidebar() {
         <div className="sidebar">
             <img src={SideImg} alt="" />
             <ul>
-                <li><h3>Home</h3></li>
-                <li><h3>About</h3></li>
-                <li><h3>Skills</h3></li>
-                <li><h3>Portifolio</h3></li>
-                <li><h3>Contact</h3></li>
+                <NavLink to="/"><li><h3>Home</h3></li></NavLink>
+                <NavLink to="/about"><li><h3>About</h3></li></NavLink>
+                <NavLink to="/skills"><li><h3>Skills</h3></li></NavLink>
+                <NavLink to="/portifolio"><li><h3>Portifolio</h3></li></NavLink>
+                <NavLink to="/contact"><li><h3>Contact</h3></li></NavLink>
             </ul>
             <h4 className="sidebarTitle">@2022-2023</h4>
             <h4>By ♪𝔸𝕊𝔻</h4>
